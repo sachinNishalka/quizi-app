75ba6702e066960a05b00ec10a4919a248264936
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-04-11 12:35:48 +0530

    removed the list from main

--- lib/main.dart
+++ lib/main.dart
@@ -32,17 +32,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List <Widget> iconList  = [];
 
-  List <String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
 
-  List <bool> answeres = [
-    false,
-    true,
-    true,
-  ];
 
 
   int qNumber = 0;
